24af22d2b07198cc9ad3a7c8618c553e1bf9c53e
Muhammad-Umair-0 <Malikmu1321@gmail.com>
2025-07-20 22:44:12 +0500

    updated

---
+++ YT project/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+
+
+app  = FastAPI()
+
+
+
+@app.get("/")
+async def index():
+    # return {'data': {"name":"Umair"}}
+    return {'data': 'blog list'}
+    # a = input("Enter a number ")
+    return {"a"}
+    # return {"sum": 2+4}
+
+
+
+@app.get("/blog/unpublished")
+def unpublished():
+    return {'data': "all unpublished "}
+
+
+@app.get("/blog/{id}")
+async def show(id : int ):
+    # fetch blod with id = id
+    return {'data': id}
+
+
+
+
+@app.get('/blog/{id}/comments')
+async def comments(id):
+    # fetch comments og blog with id = id 
+    return {'data': {'1','2','3'}}
+
+
