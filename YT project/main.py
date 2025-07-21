d42308d1e3c74e4ee95be028c692f9bbe6700992
Muhammad-Umair-0 <Malikmu1321@gmail.com>
2025-07-21 23:49:57 +0500

    updated

--- YT project/main.py
+++ YT project/main.py
@@ -13,6 +13,13 @@ async def index():
     # return {"a"}
     # return {"sum": 2+4}
 
+@app.get("/blog")
+async def index(limit = 10,published:bool= True):
+    return published
+    if published:
+        return {'data': f'{limit} published blog from DB'}
+    else:
+        return {'data': f'{limit} blog from DB'}
 
 
 @app.get("/blog/unpublished")
